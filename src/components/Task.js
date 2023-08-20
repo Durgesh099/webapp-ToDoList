7a872186b7cde4a5e804fe179f6b1e1ecf77011a
Durgesh <durgeshsonarjee@gmail.com>
2023-08-21 00:42:51 +0530

    Added delete button and changed insertbox style

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import './Task.css';
+import EditIcon from '@rsuite/icons/Edit'
+import TrashIcon from '@rsuite/icons/Trash';
 import InsertBox from "./InsertBox";
+import './Task.css';
+
+const Edit = ({ color }) => <EditIcon style={{ marginRight:10, color}}/>
+const Delete = ({ color }) => <TrashIcon style={{ marginRight:10, color}} />
 
 const Task=()=>{
 
 // ADD & RENDER TASKS ON SCREEN
     const[newState, setState]=useState([
-        {c:true, value:'First Task'},
-        {c:false, value:'Second Task'},
-        {c:false, value:'Third Task'}
+        {id:1 ,c:true, value:'First Task'},
+        {id:2 ,c:false, value:'Second Task'},
+        {id:3 ,c:false, value:'Third Task'}
     ])
 
     let str = {}
@@ -35,17 +40,35 @@ const Task=()=>{
         }
     }
 
+//Delete Task
+const del=(t)=>{
+    return function(){
+        let obj = newState.findIndex((x =>x.value===t.value))
+        for(let i=obj; i<newState.length-1; i++){
+            newState[i]=newState[i+1]
+        }
+        newState.pop()
+        const newArr = [...newState]
+        setState(newArr)
+    }
+}
+
     return(
         <div className="tasks">
         <form>
             {newState.map(t=>{
                 return(
                     <React.Fragment>
-                    <h2 className="h2" onClick={toggle(t)}>
-                        <input id='checkbox' type="checkbox" className="cbf" checked={t.c}/>
-                        <span className="text">{t.value}</span>
-                        {console.log(document.getElementById(''))}
-                    </h2>
+                    <div className="task">
+                        <h2 id="h2_1" className="TaskName" onClick={toggle(t)}>
+                            <input type="checkbox" className="cbf" checked={t.c} onChange={e => {}}/>
+                            <span key={t.id} className="text">{t.value}</span>
+                        </h2>
+                        <h2 id="h2_2" className="icons">
+                            <Edit color="blue" />
+                            <span onClick={del(t)}><Delete color="red" onClick={del(t)}/></span>
+                        </h2>
+                    </div>
                     </React.Fragment>
                 )
             })}
